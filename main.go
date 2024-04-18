24ccb10cb8d4ae0b52768cef3c50a411eebb0e08
seoed2023@proton.me <seoed2023@proton.me>
2024-04-18 15:02:44 +0800

    修改成一个目录

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
 	"getfile/utils"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"regexp"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -39,9 +43,34 @@ func main() {
 	fmt.Println("file server run at 1688 port")
 	fmt.Println("telegram: @echoty")
 
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("listen: %s\n", err)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	gracefulExitWeb(srv)
+}
+
+// 优雅退出
+func gracefulExitWeb(server *http.Server) {
+	quit := make(chan os.Signal, 4)
+	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	sig := <-quit
+
+	fmt.Println("got a signal\ngetfile server stoped", sig)
+
+	now := time.Now()
+	cxt, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(cxt); err != nil {
+		fmt.Println("err", err)
 	}
+
+	// 看看实际退出所耗费的时间
+	fmt.Println("------exited--------", time.Since(now))
+	os.Exit(0)
 }
 
 // 读取token
@@ -59,39 +88,38 @@ func handleToken(w http.ResponseWriter, r *http.Request) {
 func handleFile(w http.ResponseWriter, r *http.Request) {
 	var (
 		/*
-			http://xx.xx.xx.xx:xx/getfile/ff_b/release/aadddddd
+			http://xx.xx.xx.xx:xx/getfile/package/token字符串
 		*/
-		dirlen = 10
+		dirlen = 10 // 文件夹名长度限制
 		path   = strings.TrimPrefix(r.URL.Path, "/")
 		parts  = strings.Split(path, "/")
 	)
 
-	if len(parts) != 4 || !strings.HasPrefix(parts[0], "getfile") {
+	if len(parts) != 3 || !strings.HasPrefix(parts[0], "getfile") {
 		http.NotFound(w, r)
 		return
 	}
 
-	if parts[1] == "" || parts[2] == "" || parts[1] == "code" ||
-		strings.Contains(parts[1], ".") || strings.Contains(parts[2], ".") {
+	if parts[1] == "" || parts[2] == "" || parts[1] == "code" || strings.Contains(parts[1], ".") {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("illegal request"))
 		return
 	}
 
-	if len(parts[1]) > dirlen || len(parts[2]) > dirlen {
+	if len(parts[1]) > dirlen {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("too loooooooooog")) //target project dir name too long
 		return
 	}
 
-	if !regex.MatchString(parts[1]) || !regex.MatchString(parts[2]) {
+	if !regex.MatchString(parts[1]) {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("error path 001"))
 		return
 	}
 
 	for _, v := range sensitive {
-		if parts[1] == v || parts[2] == v {
+		if parts[1] == v {
 			w.WriteHeader(http.StatusNotFound)
 			w.Write([]byte("error path 002"))
 			return
@@ -105,13 +133,13 @@ func handleFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if parts[3] != token.(string) {
+	if parts[2] != token.(string) {
 		w.WriteHeader(http.StatusNotFound)
 		w.Write([]byte("token not correct"))
 		return
 	}
 
-	file, err := utils.ArchiveZip(fmt.Sprintf("%s/%s", parts[1], parts[2]))
+	file, err := utils.ArchiveZip(parts[1])
 	if err != nil {
 		w.Write([]byte("archive zip faild"))
 	} else if file == "" {
